58e470908b00d9ffc45498bc6b0eaa450df51295
kimmoongi <moongi0320@naver.com>
2025-07-24 13:22:26 +0900

    chore(prettier): Prettier 설정 생성

---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+const prettierConfig = {
+    "semi" : true,
+    "singleQuote" : true,
+    "tabWidth": 2,
+    "useTabs": false,
+    "trailingComma": "es5",
+    "bracketSpacing": true,
+    "arrowParens": "always",
+    "printWidth": 100,
+    "endOfLine": "lf",
+    "jsxSingleComma": false,
+    "quoteProps": "as-needed"
+};
+
+export default prettierConfig;
