ae941090ea87fe2d9079f83c222d5b5a58d44254
chael-in <cofls3511@gmail.com>
2025-07-27 03:17:21 +0900

    [0.3.0] SW-34 로고 퍼블리싱 (#3)
    * Initial commit from Create Next App
    
    * chore(prettier): Prettier 설정 생성
    
    * [SW-34] feat: 로고 컴포넌트 퍼블리싱
    
    * [SW-34] feat: 로고 컴포넌트 퍼블리싱
    
    * [SW-34] refactor: 로고 컴포넌트 leading 통합
    
    * [SW-34] refactor: 불필요한 클래스 제거
    
    ---------
    
    Co-authored-by: kimmoongi <moongi0320@naver.com>

---
+++ src/components/ui/logo.tsx
@@ -0,0 +1,46 @@
+import { cva, type VariantProps } from 'class-variance-authority'
+import { cn } from '@/lib/utils'
+import { HTMLAttributes, forwardRef } from 'react'
+
+const logoVariants = cva(
+    "text-blue-500 font-bold",
+    {
+        variants: {
+            size: {
+                xs: "text-xs leading-none",
+                sm: "text-sm leading-tight",
+                base: "text-base leading-normal",
+                lg: "text-lg leading-7",
+                xl: "text-xl leading-loose",
+                '2xl': "text-2xl leading-loose",
+                '3xl': "text-3xl leading-9",
+            }
+        },
+        defaultVariants: {
+            size: 'base',
+        }
+    }
+)
+
+type LogoProps = HTMLAttributes<HTMLSpanElement> &
+    VariantProps<typeof logoVariants> & {
+    className?: string
+}
+
+const Logo = forwardRef<HTMLSpanElement, LogoProps>(
+    ({ className, size, ...props }, ref) => {
+        return (
+            <span
+                ref={ref}
+                className={cn(logoVariants({ size }), className)}
+                {...props}
+            >
+        SHELL WE
+      </span>
+        )
+    }
+)
+
+Logo.displayName = 'Logo'
+
+export default Logo
