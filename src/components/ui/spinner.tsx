bf058e52cb290860d7e165b2a067fca01c67cbef
chael-in <cofls3511@gmail.com>
2025-07-31 02:33:56 +0900

    [0.3.0] SW-35 스피너 퍼블리싱 (#10)
    * [0.3.0] feat: spinner 컴포넌트 퍼블리싱
    
    * SW-35 refactor(spinner): 코드 최적화
    
    * SW-35 refactor(spinner): props의 className을 컴포넌트에 적용
    
    ---------
    
    Co-authored-by: Son Daehyeon <sondaehyeon01@gmail.com>

---
+++ src/components/ui/spinner.tsx
@@ -0,0 +1,28 @@
+import { type HTMLAttributes, forwardRef } from 'react';
+
+import { type VariantProps, cva } from 'class-variance-authority';
+
+import { cn } from '@/lib/utils';
+
+const spinnerVariants = cva('animate-spin rounded-full border-neutral-200 border-t-neutral-700', {
+  variants: {
+    size: {
+      small: 'h-6 w-6 border-4',
+      default: 'h-8 w-8 border-5',
+      large: 'h-10 w-10 border-6',
+    },
+  },
+  defaultVariants: {
+    size: 'default',
+  },
+});
+
+type SpinnerProps = HTMLAttributes<HTMLDivElement> & VariantProps<typeof spinnerVariants>;
+
+const Spinner = forwardRef<HTMLDivElement, SpinnerProps>(({ className, size, ...props }, ref) => {
+  return <div ref={ref} className={cn(spinnerVariants({ size }), className)} {...props} />;
+});
+
+Spinner.displayName = 'Spinner';
+
+export default Spinner;
