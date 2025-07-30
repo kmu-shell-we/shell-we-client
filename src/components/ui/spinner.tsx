3c4f47289a867b34c27b73114221d52a2c17a027
Son Daehyeon <sondaehyeon01@gmail.com>
2025-07-31 02:29:40 +0900

    SW-35 refactor(spinner): 코드 최적화

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -1,66 +1,28 @@
-"use client"
+import { type HTMLAttributes, forwardRef } from 'react';
 
-import { cva, type VariantProps } from "class-variance-authority"
-import { cn } from "@/lib/utils"
-import { forwardRef, type HTMLAttributes } from "react"
+import { type VariantProps, cva } from 'class-variance-authority';
 
-const spinnerWrapperVariants = cva("relative", {
-    variants: {
-        size: {
-            small: "w-6 h-6",
-            default: "w-8 h-8",
-            large: "w-10 h-10",
-        },
+import { cn } from '@/lib/utils';
+
+const spinnerVariants = cva('animate-spin rounded-full border-neutral-200 border-t-neutral-700', {
+  variants: {
+    size: {
+      small: 'h-6 w-6 border-4',
+      default: 'h-8 w-8 border-5',
+      large: 'h-10 w-10 border-6',
     },
-    defaultVariants: {
-        size: "default",
-    },
-})
+  },
+  defaultVariants: {
+    size: 'default',
+  },
+});
 
-const spinnerTrackVariants = cva(
-    "rounded-full border-neutral-200 absolute",
-    {
-        variants: {
-            size: {
-                small: "w-6 h-6 border-4",
-                default: "w-8 h-8 border-[5px]",
-                large: "w-10 h-10 border-[6px]",
-            },
-        },
-    }
-)
+type SpinnerProps = HTMLAttributes<HTMLDivElement> & VariantProps<typeof spinnerVariants>;
 
-const spinnerArcVariants = cva(
-    "absolute inset-0 rounded-full border-t-neutral-700 border-r-transparent border-b-transparent border-l-transparent animate-spin",
-    {
-        variants: {
-            size: {
-                small: "border-4",
-                default: "border-[5px]",
-                large: "border-[6px]",
-            },
-        },
-    }
-)
+const Spinner = forwardRef<HTMLDivElement, SpinnerProps>(({ className, size, ...props }, ref) => {
+  return <div ref={ref} className={cn(spinnerVariants({ size }))} {...props} />;
+});
 
-type SpinnerProps = HTMLAttributes<HTMLDivElement> &
-    VariantProps<typeof spinnerWrapperVariants>
+Spinner.displayName = 'Spinner';
 
-const Spinner = forwardRef<HTMLDivElement, SpinnerProps>(
-    ({ className, size, ...props }, ref) => {
-        return (
-            <div
-                ref={ref}
-                className={cn(spinnerWrapperVariants({ size }), className)}
-                {...props}
-            >
-                <div className={cn(spinnerTrackVariants({ size }))} />
-                <div className={cn(spinnerArcVariants({ size }))} />
-            </div>
-        )
-    }
-)
-
-Spinner.displayName = "Spinner"
-
-export default Spinner
+export default Spinner;
