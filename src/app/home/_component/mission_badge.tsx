92f30950e8c3c9fa82ca47ede4b4cd2265d60af8
Moonki Kim <84397561+Kimmoongi0320@users.noreply.github.com>
2025-08-03 00:11:42 +0900

    [0.4.0] [SW-76] chore(MissionBadge): Mission Badge 컴포넌트 퍼블리싱 및 HeroIcon 설정 추가 (#12)
    * [SW-76] chore(MissionBadge): Mission Badge 컴포넌트 퍼블리싱 및 HeroIcon 설정 추가
    
    * [SW-76] refactor(MissionBadge): MissionBadge 속성 타입 변경 및 색상 type 파일 분리

---
+++ src/app/home/_component/mission_badge.tsx
@@ -0,0 +1,48 @@
+import { ComponentType, SVGProps } from 'react';
+
+import { MoonIcon } from '@heroicons/react/20/solid';
+import { TicketIcon } from '@heroicons/react/20/solid';
+import { RocketLaunchIcon } from '@heroicons/react/20/solid';
+
+import { TypeColor } from '@/app/types/colors';
+
+import Badge from '@/components/ui/badge';
+
+type MissionType = 'DAILY' | 'WEEKLY' | 'SPECIAL';
+interface Props {
+  type: MissionType;
+}
+
+type MissionInfo = {
+  icon: ComponentType<SVGProps<SVGSVGElement>>;
+  text: string;
+  color: TypeColor;
+};
+
+const missions: Record<MissionType, MissionInfo> = {
+  DAILY: {
+    icon: MoonIcon,
+    text: '일간 미션',
+    color: 'white',
+  },
+  WEEKLY: {
+    icon: TicketIcon,
+    text: '주간 미션',
+    color: 'white',
+  },
+  SPECIAL: {
+    icon: RocketLaunchIcon,
+    text: '특별 미션',
+    color: 'success',
+  },
+} as const;
+
+export default function MissionBadge({ type }: Props) {
+  const { icon: Icon, text, color } = missions[type];
+  return (
+    <Badge size="xs" color={color}>
+      <Icon className="h-3 w-3" />
+      {text}
+    </Badge>
+  );
+}
