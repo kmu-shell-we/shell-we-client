7da88905dbcb65936d9634d7c4c88c3613ebc003
Kimmoongi0320 <moongi0320@naver.com>
2025-08-07 12:42:16 +0900

    [SW-74] refactor(homePage): slidebar 옵션 수정

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import { useEffect, useRef, useState } from 'react';
-
 import { ClockIcon, PlusIcon } from '@heroicons/react/20/solid';
 
 import Mission from '@/app/home/_component/mission';
@@ -10,42 +8,6 @@ import TeamRankCard from '@/app/home/_component/team_rank_card';
 import RoundedBadge from '@/components/ui/rounded-badge';
 
 export default function HomePage() {
-  const sliderRef = useRef<HTMLDivElement>(null);
-  const [isDragging, setIsDragging] = useState(false);
-  const [startX, setStartX] = useState(0);
-  const [scrollLeft, setScrollLeft] = useState(0);
-
-  useEffect(() => {
-    const slider = sliderRef.current;
-    if (!slider) return;
-
-    const handleMouseDown = (e: MouseEvent) => {
-      setIsDragging(true);
-      setStartX(e.pageX - slider.offsetLeft);
-      setScrollLeft(slider.scrollLeft);
-    };
-    const handleMouseLeave = () => setIsDragging(false);
-    const handleMouseUp = () => setIsDragging(false);
-    const handleMouseMove = (e: MouseEvent) => {
-      if (!isDragging) return;
-      e.preventDefault();
-      const x = e.pageX - slider.offsetLeft;
-      const walk = x - startX;
-      slider.scrollLeft = scrollLeft - walk;
-    };
-
-    slider.addEventListener('mousedown', handleMouseDown);
-    slider.addEventListener('mouseleave', handleMouseLeave);
-    slider.addEventListener('mouseup', handleMouseUp);
-    slider.addEventListener('mousemove', handleMouseMove);
-    return () => {
-      slider.removeEventListener('mousedown', handleMouseDown);
-      slider.removeEventListener('mouseleave', handleMouseLeave);
-      slider.removeEventListener('mouseup', handleMouseUp);
-      slider.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, [isDragging, startX, scrollLeft]);
-
   return (
     <div className="flex w-94 flex-col gap-y-6 bg-white px-6">
       {/*Todo: 남은 기간 바꾸기*/}
@@ -59,7 +21,7 @@ export default function HomePage() {
       <div className="flex flex-col gap-y-4">
         <span className="text-xl font-semibold">미션 목록</span>
 
-        <div className="scrollbar-hide overflow-x-auto" ref={sliderRef}>
+        <div className="scrollbar-hide overflow-x-auto">
           <div className="flex gap-4">
             <div className="w-50 flex-shrink-0">
               <Mission
