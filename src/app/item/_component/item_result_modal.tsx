b6524795f5660561a66df12a7c84b6314db2d1e0
kimmoongi <moongi0320@naver.com>
2025-08-10 15:39:57 +0900

    [SW-101] feat(itemResult): 아이템 사용 결과 모달 생성

---
+++ src/app/item/_component/item_result_modal.tsx
@@ -0,0 +1,102 @@
+import { ComponentType, SVGProps } from 'react';
+
+import { ItemType } from '@/types/item';
+import {
+  ArrowDownIcon,
+  ArrowPathIcon,
+  BeakerIcon,
+  BoltIcon,
+  StarIcon,
+  XMarkIcon,
+} from '@heroicons/react/20/solid';
+
+import Modal from '@/components/modal/modal';
+import Button from '@/components/ui/button';
+
+interface ItemModalProps {
+  type: ItemType;
+  open: boolean;
+  onClose: () => void;
+}
+
+type ItemInfo = {
+  icon: ComponentType<SVGProps<SVGSVGElement>>;
+  title: string;
+  description: string;
+  bgColor: string;
+  color: string;
+};
+
+const items: Record<ItemType, ItemInfo> = {
+  double: {
+    icon: BoltIcon,
+    title: '경험치 2배',
+    description: '다음 미션 경험치 2배',
+    bgColor: 'bg-green-50',
+    color: 'text-green-500',
+  },
+  reset: {
+    icon: ArrowPathIcon,
+    title: '미션 초기화',
+    description: '오늘의 미션 하나 초기화',
+    bgColor: 'bg-amber-50',
+    color: 'text-amber-500',
+  },
+  attack: {
+    icon: BeakerIcon,
+    title: '점수 깍기',
+    description: '랜덤 팀 점수 50p 깍기',
+    bgColor: 'bg-red-50',
+    color: 'text-red-500',
+  },
+  boom: {
+    icon: XMarkIcon,
+    title: '꽝',
+    description: '아무것도 얻지 못했습니다',
+    bgColor: 'bg-gray-50',
+    color: 'text-gray-500',
+  },
+};
+
+export default function ItemResultModal({ type, open, onClose }: ItemModalProps) {
+  const { icon: Icon, title, description, bgColor, color } = items[type];
+
+  if (!open) return null;
+  return (
+    <Modal>
+      <div className="flex w-75 flex-col items-center gap-y-4 rounded-xl bg-white px-6 py-6">
+        <div className={`flex items-center justify-center rounded-full ${bgColor} h-16 w-16`}>
+          <Icon className={`h-6 w-6 ${color}`} />
+        </div>
+
+        <div className="flex flex-col items-center">
+          <span className="text-xl font-semibold">{title}</span>
+          <span className="text-sm font-medium text-gray-600">{description}</span>
+        </div>
+
+        {type == 'attack' && (
+          <div className={`flex flex-col gap-y-1 rounded-xl ${bgColor} w-full items-center py-4`}>
+            <span className="font-semibold text-gray-700">팀 이름</span>
+            <div className={`flex items-center text-xl font-semibold ${color}`}>
+              <span>50</span>
+              <StarIcon className="h-5 w-5" />
+            </div>
+          </div>
+        )}
+
+        {type == 'reset' && (
+          <div className={`flex flex-col gap-y-1 rounded-xl ${bgColor} w-full items-center py-4`}>
+            <span className="font-semibold text-gray-700">이전 미션 이름</span>
+            <ArrowDownIcon className="h-5 w-5" />
+            <span className="font-semibold text-gray-700">새로운 미션 이름</span>
+            <span className="text-sm font-normal text-gray-500">미션 설명</span>
+          </div>
+        )}
+
+        <Button color="white" size="small" outlined={true} onClick={onClose} className="w-full">
+          확인
+        </Button>
+      </div>
+    </Modal>
+  );
+}
