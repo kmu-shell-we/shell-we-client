da526a6c8d19d5d77a72142a3f5503be627c9336
chael-in <cofls3511@gmail.com>
2025-08-12 00:27:28 +0900

    [0.5.0] SW-108 갤러리 페이지 퍼블리싱 (#30)
    * [SW-108] feat: 갤러리 페이지 퍼블리싱
    
    * [SW-108] feat: 갤러리 아이템 클릭 시 상세 페이지로 이동하는 기능 추가
    
    * [SW-108] refactor(gallery): Gallery Detail 페이지 라우팅을 teamName 대신 team.id로 변경
    
    * [SW-108] refactor: encodeURIComponent 제거
    
    * [SW-108] refactor: self-stretch 제거

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import Link from 'next/link';
+
+import type { TeamBlock } from '@/types/gallery';
+
+import GalleryItem from '@/app/gallery/_component/gallery-item';
+
+export default function GalleryPage() {
+  const teams: TeamBlock[] = [];
+
+  return (
+    <div className="flex flex-col items-start gap-6 px-6">
+      {teams.map((t) => (
+        <Link key={t.id} href={`/gallery-detail/${t.id}`}>
+          <GalleryItem teamName={t.teamName} photos={t.photos} />
+        </Link>
+      ))}
+    </div>
+  );
+}
