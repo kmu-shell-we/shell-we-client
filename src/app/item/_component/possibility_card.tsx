619f93ef90ba35473682bfecaa78f6a17beb7b8d
kimmoongi <moongi0320@naver.com>
2025-08-13 10:42:25 +0900

    [SW-97] refactor(possibilityCard): 아이템 확률 카드 컴포넌트의 속성을 외부에서 전달 받도록 변경

--- src/app/item/_component/possibility_card.tsx
+++ src/app/item/_component/possibility_card.tsx
@@ -1,11 +1,6 @@
 import { ComponentType, SVGProps } from 'react';
 
-import { ItemType } from '@/types/item';
-import { ArrowPathIcon, BeakerIcon, BoltIcon, XMarkIcon } from '@heroicons/react/20/solid';
-
-interface Props {
-  type: ItemType;
-}
+import { cn } from '@/lib/utils';
 
 type ItemInfo = {
   icon: ComponentType<SVGProps<SVGSVGElement>>;
@@ -16,49 +11,19 @@ type ItemInfo = {
   color: string;
 };
 
-const items: Record<ItemType, ItemInfo> = {
-  double: {
-    icon: BoltIcon,
-    title: '경험치 2배',
-    description: '다음 미션 경험치 2배',
-    percent: 25,
-    bgColor: 'bg-green-50',
-    color: 'text-green-500',
-  },
-  reset: {
-    icon: ArrowPathIcon,
-    title: '미션 초기화',
-    description: '오늘의 미션 하나 초기화',
-    percent: 15,
-    bgColor: 'bg-amber-50',
-    color: 'text-amber-500',
-  },
-  attack: {
-    icon: BeakerIcon,
-    title: '점수 깍기',
-    description: '랜덤 팀 점수 50p 깍기',
-    percent: 10,
-    bgColor: 'bg-red-50',
-    color: 'text-red-500',
-  },
-  boom: {
-    icon: XMarkIcon,
-    title: '꽝',
-    description: '아무것도 얻지 못했습니다',
-    percent: 50,
-    bgColor: 'bg-gray-50',
-    color: 'text-gray-500',
-  },
-};
-
-export default function PossibilityCard({ type }: Props) {
-  const { icon: Icon, title, description, percent, bgColor, color } = items[type];
-
+export default function PossibilityCard({
+  icon: Icon,
+  title,
+  description,
+  percent,
+  bgColor,
+  color,
+}: ItemInfo) {
   return (
-    <div className="flex justify-between">
+    <div className="flex items-center justify-between">
       <div className="flex items-center gap-4">
-        <div className={`flex items-center justify-center rounded-full ${bgColor} h-16 w-16`}>
-          <Icon className={`h-6 w-6 ${color}`} />
+        <div className={cn('flex h-16 w-16 items-center justify-center rounded-full', bgColor)}>
+          <Icon className={cn('h-6 w-6', color)} />
         </div>
 
         <div className="flex flex-col">
