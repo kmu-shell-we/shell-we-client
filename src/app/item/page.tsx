a898857912abaaf99a916f19ba1d360b0744848a
Kimmoongi0320 <moongi0320@naver.com>
2025-08-13 16:25:06 +0900

    [SW-97] refactor(itemPage): 아이템 페이지 룰렛 컴포넌트 추가 및 확률 카드 속성값 변환

--- src/app/item/page.tsx
+++ src/app/item/page.tsx
@@ -1,6 +1,8 @@
-import { ClockIcon } from '@heroicons/react/20/solid';
+import { ArrowPathIcon, BeakerIcon, ClockIcon, XMarkIcon } from '@heroicons/react/20/solid';
+import { BoltIcon } from '@heroicons/react/20/solid';
 
 import PossibilityCard from '@/app/item/_component/possibility_card';
+import Roulette from '@/app/item/_component/roulette';
 
 import ItemMenu from '@/components/layout/item-menu';
 import RoundedBadge from '@/components/ui/rounded-badge';
@@ -17,8 +19,9 @@ export default function ItemPage() {
           </RoundedBadge>
         </div>
 
+        <Roulette />
+
         <div className="flex flex-col items-center gap-y-6">
-          {/*TODO:추후에 룰렛 컴포넌트 삽입*/}
           <div>
             <span className="font-normal text-gray-500">비용: </span>
             <span className="font-semibold text-gray-700">100P</span>
@@ -28,10 +31,38 @@ export default function ItemPage() {
         <div className="flex w-full flex-col gap-y-4">
           <span className="text-xl font-semibold">확률</span>
           <div>
-            <PossibilityCard type="double" />
-            <PossibilityCard type="reset" />
-            <PossibilityCard type="attack" />
-            <PossibilityCard type="boom" />
+            <PossibilityCard
+              icon={BoltIcon}
+              title="경험치 2배"
+              description="다음 미션 경험치 2배"
+              percent={25}
+              bgColor="bg-green-50"
+              color="text-green-500"
+            />
+            <PossibilityCard
+              icon={ArrowPathIcon}
+              title="미션 초기화"
+              description="오늘의 미션 하나 초기화"
+              percent={15}
+              bgColor="bg-amber-50"
+              color="text-amber-500"
+            />
+            <PossibilityCard
+              icon={BeakerIcon}
+              title="점수 깍기"
+              description="랜덤 팀 점수 50p 깍기"
+              percent={10}
+              bgColor="bg-red-50"
+              color="text-red-500"
+            />
+            <PossibilityCard
+              icon={XMarkIcon}
+              title="꽝"
+              description="아무것도 얻지 못했습니다"
+              percent={50}
+              bgColor="bg-gray-50"
+              color="text-gray-500"
+            />
           </div>
         </div>
       </div>
