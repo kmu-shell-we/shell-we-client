af159998f131ab2feb52e25c4c198188f376bae2
chael-in <cofls3511@gmail.com>
2025-08-21 00:39:06 +0900

    [SW-106] feat: hour 컴포넌트 구현

---
+++ src/app/team/_component/schedule/hour.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import Cell from '@/app/team/_component/schedule/cell';
+
+type HourProps = {
+  hour: number;
+  counts: Record<string, number>;
+};
+
+export default function Hour({ hour, counts }: HourProps) {
+  const h = String(hour).padStart(2, '0');
+  const time1 = `${h}:00`;
+  const time2 = `${h}:30`;
+
+  return (
+    <div className="flex flex-col items-start justify-start">
+      <div className="h-5 self-stretch">
+        <Cell count={counts[time1]} />
+      </div>
+      <div className="h-0 outline outline-1 outline-offset-[-0.50px] outline-neutral-300" />
+      <div className="h-5 self-stretch">
+        <Cell count={counts[time2]} />
+      </div>
+    </div>
+  );
+}
