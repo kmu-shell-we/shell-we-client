e8475c62793ef61296947423cc8f81447649b9d8
kimmoongi <moongi0320@naver.com>
2025-08-27 20:16:06 +0900

    [SW-131] feat(AuthPage):wink 로그인 페이지 리다이렉트 생성

--- src/api/controller/auth.ts
+++ src/api/controller/auth.ts
@@ -1,9 +1,5 @@
-import ApiRequest from '@/api/request';
-
 export default class Auth {
-  constructor(private readonly request: ApiRequest) {}
-
   public async login(): Promise<void> {
-    return this.request.get('/auth/oauth/wink');
+    window.location.href = 'http://localhost:8080/api/auth/oauth/wink';
   }
 }
