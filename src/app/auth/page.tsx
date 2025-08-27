a8ae5088fd901241582f82cecc09b3c233ff18b8
kimmoongi <moongi0320@naver.com>
2025-08-27 18:22:47 +0900

    [SW-131] feat(AuthPage):auth 페이지 시작하기 버튼과 api 연결

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,13 +1,16 @@
 'use client';
 
-import Link from 'next/link';
-
+import { useApi } from '@/hook/use-api';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 
 import Button from '@/components/ui/button';
 import Logo from '@/components/ui/logo';
 
+import Api from '@/api';
+
 export default function Page() {
+  const [, startApi] = useApi();
+
   return (
     <div className="flex h-[100dvh] w-screen flex-col items-center justify-center gap-60 px-6">
       <div className="flex items-center gap-5">
@@ -15,13 +18,17 @@ export default function Page() {
         <p className="text-2xl font-light text-gray-500">Everything</p>
       </div>
 
-      {/* TODO: OAuth 인증 페이지 구현 예정 */}
-      <Link href="/auth/oauth" className="flex w-full justify-center">
-        <Button className="flex w-full max-w-[320px] items-center justify-center gap-2">
-          시작하기
-          <ArrowRightIcon className="h-4 w-4" />
-        </Button>
-      </Link>
+      <Button
+        className="flex w-full max-w-[320px] items-center justify-center gap-2"
+        onClick={async () => {
+          startApi(async () => {
+            await Api.Domain.Auth.login();
+          });
+        }}
+      >
+        시작하기
+        <ArrowRightIcon className="h-4 w-4" />
+      </Button>
     </div>
   );
 }
