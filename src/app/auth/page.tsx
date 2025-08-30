61c5c92c21b4f1d41b6d8c30e51a75f4fd429da9
kimmoongi <moongi0320@naver.com>
2025-08-30 23:06:33 +0900

    [SW-131] refactor(auth): 로그인 요청 redirect 요청으로 변경

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,16 +1,13 @@
 'use client';
 
-import { useApi } from '@/hook/use-api';
+import { RedirectType, redirect } from 'next/navigation';
+
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 
 import Button from '@/components/ui/button';
 import Logo from '@/components/ui/logo';
 
-import Api from '@/api';
-
 export default function Page() {
-  const [, startApi] = useApi();
-
   return (
     <div className="flex h-[100dvh] w-screen flex-col items-center justify-center gap-60 px-6">
       <div className="flex items-center gap-5">
@@ -20,10 +17,11 @@ export default function Page() {
 
       <Button
         className="flex w-full max-w-[320px] items-center justify-center gap-2"
-        onClick={async () => {
-          startApi(async () => {
-            await Api.Domain.Auth.login();
-          });
+        onClick={() => {
+          redirect(
+            process.env['NEXT_PUBLIC_API_URL'] + 'api/auth/oauth/wink',
+            RedirectType.replace
+          );
         }}
       >
         시작하기
